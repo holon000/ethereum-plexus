e1d850d457525401057d097762e86c23241dc1a0
Taylor Gerring <taylor.gerring@gmail.com>
2014-02-18 17:49:31 +0100

    Added person card examples for styling

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,10 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'plexus'
-  }
+  },
+  '/person-cards': {
+    view: 'person-cards'
+  },
 
   /*
   // But what if you want your home page to display
