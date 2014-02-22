2791da6a09cac742633975dd16f6f0e94a386230
Taylor Gerring <taylor.gerring@gmail.com>
2014-02-22 17:25:53 +0100

    Pagination of "features"

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready( function() {
-    
+
     $(".scroll").click(function (event) {
         event.preventDefault();
         //calculate destination place
@@ -36,4 +36,47 @@ $( document ).ready( function() {
         })
         .mapster('set',true,'1,2,3,4,5');
     
+
+    $('#how button.prev').click(function(){updateFeature(-1)});
+    $('#how button.next').click(function(){updateFeature(1)});
+
+    var updateFeature = function(direction) {
+      // console.log('direction', direction);
+      //direction should be a positive or negative value to indicate how many elements to walk, i.e. 2 for forwards 2, -1 for backwards
+      var children = $('#content-circle').children();
+      // console.log(children);
+      var maxIndex = children.length - 1;
+      // console.log('maxIndex', maxIndex);
+      var currentIndex = 0;
+
+      // get shown item. does nit handle multiple shown well yet
+      $.each(children, function(index, value){
+        // console.log(index, value);
+        if (! $(value).hasClass('hidden')) {
+          currentIndex = index;
+          $(value).addClass('hidden');
+          return;
+        }
+      });
+      // console.log('currentIndex', currentIndex);
+
+      var newIndex = currentIndex + direction;
+      // console.log('newIndex', newIndex);
+      if (newIndex <= 0) {
+        // console.log('newIndex <= 0');
+        newIndex = 0;
+        $('#how button.prev').prop('disabled', true);
+      } else if (newIndex >= maxIndex) {
+        // console.log('newIndex >= maxIndex');
+        newIndex = maxIndex;
+        $('#how button.next').prop('disabled', true);
+      } else {
+        // console.log('else');
+        $('#how button.prev').prop('disabled', false);
+        $('#how button.next').prop('disabled', false);
+      }
+
+      $(children[newIndex]).removeClass('hidden');
+
+    }
 });
