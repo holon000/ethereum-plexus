502e590c23b10339c67b315073a0da6e4450bb39
Taylor Gerring <taylor.gerring@gmail.com>
2014-02-22 19:45:41 +0100

    Philosophy hover now changes text

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -28,17 +28,6 @@ $( document ).ready( function() {
         padZeroes: true,
         layout: '<b><span class="timer-unit">{dnn}</span><span class="timer-unit-desc">{dl}</span> <span class="timer-unit-sep">:</span> <span class="timer-unit">{hnn}</span> <span class="timer-unit-desc">{hl}</span> <span class="timer-unit-sep">:</span> <span class="timer-unit">{mnn}</span> <span class="timer-unit-desc">{ml}</span> <span class="timer-unit-sep">:</span> <span class="timer-unit">{snn}</span> <span class="timer-unit-desc">{sl}</span></b>',
     });
-    
-    // bind using "data-key" as a mapKey, and select four states
-    // $('#philosophymap')
-    //   .mapster({
-    //     mapKey: 'data-key'
-    //   })
-    //   .mapster('set',true,'item1,item2,item3,item4,item5')
-    //   .mapster('set_options',{
-    //     onMouseover: function(){console.log('mouse over')}
-    //     onMouseout: function(){console.log('mouse out')}
-    //   });
 
     var map = $('#philoimg');
     var inArea = false;
@@ -49,16 +38,12 @@ $( document ).ready( function() {
       isSelectable: false,
       onMouseover: function (data) {
         inArea = true;
-        console.log('onMouseover', data.key);
-
-        // $('#beatles-caption-header').text(captions[data.key][0]);
-        // $('#beatles-caption-text').text(captions[data.key][1]);
-        // $('#beatles-caption').show();
+        var target = '#' + data.key;
+        setChildrenHidden('#philotexts');
+        $(target).removeClass('hidden');
       },
       onMouseout: function (data) {
         inArea = false;
-        console.log('onMouseout', data.key);
-        // $('#beatles-caption').hide();
       }
     };
     opts = $.extend({}, all_opts, initial_opts, single_opts);
@@ -76,43 +61,41 @@ $( document ).ready( function() {
             }
         });
 
-    
+    var setChildrenHidden = function(selector) {
+      var children = $(selector).children();
+      $.each(children, function(index, value){
+        if (! $(value).hasClass('hidden')) {
+          $(value).addClass('hidden');
+        }
+      });
+    }
 
     $('#how button.prev').click(function(){updateFeature(-1)});
     $('#how button.next').click(function(){updateFeature(1)});
 
     var updateFeature = function(direction) {
-      // console.log('direction', direction);
       //direction should be a positive or negative value to indicate how many elements to walk, i.e. 2 for forwards 2, -1 for backwards
       var children = $('#content-circle').children();
-      // console.log(children);
       var maxIndex = children.length - 1;
-      // console.log('maxIndex', maxIndex);
       var currentIndex = 0;
 
       // get shown item. does nit handle multiple shown well yet
       $.each(children, function(index, value){
-        // console.log(index, value);
         if (! $(value).hasClass('hidden')) {
           currentIndex = index;
           $(value).addClass('hidden');
           return;
         }
       });
-      // console.log('currentIndex', currentIndex);
 
       var newIndex = currentIndex + direction;
-      // console.log('newIndex', newIndex);
       if (newIndex <= 0) {
-        // console.log('newIndex <= 0');
         newIndex = 0;
         $('#how button.prev').prop('disabled', true);
       } else if (newIndex >= maxIndex) {
-        // console.log('newIndex >= maxIndex');
         newIndex = maxIndex;
         $('#how button.next').prop('disabled', true);
       } else {
-        // console.log('else');
         $('#how button.prev').prop('disabled', false);
         $('#how button.next').prop('disabled', false);
       }
