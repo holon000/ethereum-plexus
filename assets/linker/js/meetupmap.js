2f30b10c3b264090171f5d1a9caaf6cc04e97904
Stephan Tual <stephan.tual@gmail.com>
2014-03-08 02:02:07 +0000

    Map is now responsive, disabled zoom

--- assets/linker/js/meetupmap.js
+++ assets/linker/js/meetupmap.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
 
-  var width = 960,
-      height = 500;
+  var margin = {left: 10, right: 10},
+     width = parseInt(d3.select('#meetupmap').style('width')) - margin.left - margin.right,
+     height = width *  0.5;
 
   var projection = d3.geo.mercator()
     .scale((width + 1) / 2 / Math.PI)
     .translate([width / 2, height / 2 + 50])
     .precision(0.1);
 
-  var svg = d3.select("#meetupmap").append("svg")
-      .attr("width", width)
-      .attr("height", height);
-
   var path = d3.geo.path()
       .projection(projection);
+
+  var svg = d3.select("#meetupmap").append("svg")
+      .attr("width", width + 'px')
+      .attr("height", height + 'px');
 
   var g = svg.append("g");
   
 
-  // load and display the World
+  // load and display the world
   d3.json("./static/world.json", function(error, topology) {
 
     // load and display the cities
@@ -43,7 +44,6 @@ $(document).ready(function() {
             );
     });
 
-
     g.selectAll("path")
           .data(topojson.feature(topology, topology.objects.countries).features)
         .enter()
@@ -52,18 +52,44 @@ $(document).ready(function() {
 
   });
 
-  // zoom and pan
-  var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 5])
-    .on("zoom",function() {
-        g.attr("transform","translate(" + d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
-        g.selectAll("path")  
-          .attr("d", path.projection(projection));
-        g.selectAll("circle")
-          .attr("r", 5 / d3.event.scale )
-          .style("stroke-width", 1 / d3.event.scale);
-  });
+    // zoom and pan
+    // var zoom = d3.behavior.zoom()
+    //   .scaleExtent([1, 5])
+    //   .on("zoom",function() {
+    //       g.attr("transform","translate(" + d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
+    //       g.selectAll("path")  
+    //         .attr("d", path.projection(projection));
+    //       g.selectAll("circle")
+    //         .attr("r", 5 / d3.event.scale )
+    //         .style("stroke-width", 1 / d3.event.scale);
+    // });
 
-  svg.call(zoom);
+   // svg.call(zoom);
+
+  // responsive 
+  d3.select(window).on('resize', resize);
+
+  function resize() {
+    width = parseInt(d3.select('#meetupmap').style('width')) - margin.left - margin.right;
+    height = width *  0.5;
+
+    // update projection
+    projection = d3.geo.mercator()
+      .scale((width + 1) / 2 / Math.PI)
+      .translate([width / 2, height / 2 + 50])
+      .precision(0.1);
+
+    // resize the map container
+    svg
+      .style('width', width + 'px')
+      .style('height', height + 'px');
+
+    // resize the map
+    g.selectAll("path").attr("d", path.projection(projection));
+    g.selectAll("circle")
+      .attr("cx", function(d) { return projection([d.lon, d.lat])[0]; })
+      .attr("cy", function(d) { return projection([d.lon, d.lat])[1]; });
+  }
+
 
 });
