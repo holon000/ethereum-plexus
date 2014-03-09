84bba2222c1eb2e9eee8c8ca25a22efe111f23dd
Stephan Tual <stephan.tual@gmail.com>
2014-03-09 20:03:00 +0000

    Map now allows zoom + auto pan on click

--- assets/linker/js/meetupmap.js
+++ assets/linker/js/meetupmap.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-  var zoomEnabled = false;
+  // var zoomEnabled = false;
+
+  var centered;
 
   var margin = {left: 10, right: 10},
      width = parseInt(d3.select('#meetupmap').style('width')) - margin.left - margin.right,
@@ -42,7 +44,7 @@ $(document).ready(function() {
           .append("circle")
             .attr("cx", function(d) { return projection([d.lon, d.lat])[0]; })
             .attr("cy", function(d) { return projection([d.lon, d.lat])[1]; })
-            .attr("r", 5)
+            .attr("r", 4)
             .style("stroke", "black")
             .style("stroke-width", 1)
             .style("fill", "#aab5de")
@@ -57,28 +59,51 @@ $(document).ready(function() {
           .data(topojson.feature(topology, topology.objects.countries).features)
         .enter()
           .append("path")
-          .attr("d", path);
+          .attr("d", path)
+          .on("click", clicked);
 
   });
 
 
-  var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 5])
-    .on("zoom",function() {
-        if (zoomEnabled) {
-          g.attr("transform","translate(" + d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
-          g.selectAll("path")  
-            .attr("d", path.projection(projection));
-          g.selectAll("circle")
-            .attr("r", 5 / d3.event.scale )
-            .style("stroke-width", 1 / d3.event.scale);
-        } else {return false;}
-  });
+  // var zoom = d3.behavior.zoom()
+  //   .scaleExtent([1, 5])
+  //   .on("zoom",function() {
+  //       if (zoomEnabled) {
+  //         g.attr("transform","translate(" + d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
+  //         g.selectAll("path")  
+  //           .attr("d", path.projection(projection));
+  //         g.selectAll("circle")
+  //           .attr("r", 5 / d3.event.scale )
+  //           .style("stroke-width", 1 / d3.event.scale);
+  //       } else {return false;}
+  // });
 
-  function clicked() {
-    zoomEnabled = !zoomEnabled;
-    if (zoomEnabled) svg.call(zoom);
+
+  function clicked(d) {
+    var x, y, k;
+
+    if (d && centered !== d) {
+      var centroid = path.centroid(d);
+      x = centroid[0];
+      y = centroid[1];
+      k = 2;
+      centered = d;
+    } else {
+      x = width / 2;
+      y = height / 2;
+      k = 1;
+      centered = null;
+    }
+
+    g.selectAll("path")
+        .classed("active", centered && function(d) { return d === centered; });
+
+    g.transition()
+        .duration(750)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .style("stroke-width", 1.5 / k + "px");
   }
+
 
 
   // responsive 
